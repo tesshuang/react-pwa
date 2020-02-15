154255dc0ecc25dc92dde3e38d82a344d6d80745
Tess Huang <tesshuangxj@yahoo.com>
2020-02-14 16:05:02 -0800

    Updates

---
+++ precache-manifest.8f9876e101d3981199b7144e3342a42b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8728a23d29cbfa5db7ef65971471fb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99c34f74bd96aeae44c4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "36489d5999b6a4252aaf",
+    "url": "/static/js/2.e37a3485.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e37a3485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c34f74bd96aeae44c4",
+    "url": "/static/js/main.a336e15d.chunk.js"
+  },
+  {
+    "revision": "a1f32b489c8c7b15723a",
+    "url": "/static/js/runtime-main.92948362.js"
+  }
+]);
